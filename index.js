4ee4fec76abc71d7665f042a80c644a20dd83a8e
Canh Tran <canh.tran@wizeline.com>
2018-06-22 14:46:10 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const crypto = require('./crytobject');
+
+const input = {a: 1, b: 2};
+
+input_encrypted = crypto.encrypted(input);
+
+console.log("Encrypted");
+console.log(input_encrypted);
+
+output_decrypted = crypto.decrypted(input_encrypted);
+
+console.log("Decrypted");
+console.log(output_decrypted);
